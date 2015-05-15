ca730b4587be490cbb0566999d5bffd5e5b9ea5b
Carl Osterwisch <costerwi@gmail.com>
2015-05-15 13:27:06 -0400

    Updated README to include the examples.

--- examples/ipython_notebook_config.py
+++
@@ -1,6 +0,0 @@
-# Configuration file for ipython-notebook.
-
-c = get_config()
-
-c.NotebookApp.contents_manager_class = 'notebookdiff.filemanager.NotebookDiffContentsManager'
-c.NotebookApp.server_extensions = [ 'notebookdiff' ]
